8d07364f837c212ce0879fb1414021db3ee8595b
vtamap <vtamap@gmail.com>
2026-01-11 18:23:24 -0500

    Commit 1: Estructura para la prueba tecnica teniendo en cuenta el Backend con Java-SpringBoot y Frontend solicitado con Angular

---
+++ nomina-backend/src/main/java/com/banco/nominabackend/NominaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.banco.nominabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NominaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NominaBackendApplication.class, args);
+    }
+
+}
---
+++ nomina-backend/src/test/java/com/banco/nominabackend/NominaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banco.nominabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NominaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
